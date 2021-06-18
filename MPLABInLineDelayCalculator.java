b327605b745e488476225f084390010115d2a911
David Harmon <harmondrum@gmail.com>
2021-06-18 14:51:39 -0600

    made some edits to java code and got started on a calculator for the pic prescaler and post scaler values

--- MPLABInLineDelayCalculator.java
+++ MPLABInLineDelayCalculator.java
@@ -1,5 +1,50 @@
+/*
+* David Harmon
+* 6-18-21
+*
+*this calculator is used for the purpose of allowing a user to enter in hardware
+*calculations into this program for MPLAB and have the calculator calculate
+*what registers should be used and the values to create the delay needed.
+*
+*
+*/
+
+
+
 public class MPLABInLineDelayCalculator {
-    public static void main(String[] args){
-        System.out.println("Hello World");
+/*
+    public static void toTheNearest(double nearest, double input){
+        int found = 0;
+        for(int i = 0; i < input.length(); i++){
+            if (input.charAt(i) == "."){
+                found = -1;
+            }
+        }
+
+        if (found == -1){
+            System.out.println("The input is a decimal");
+        }
+
+
     }
+*/
+
+
+     public static void main(String[] args){
+         double test = 3.14159;
+         String testString = Double.toString(test);
+        //toTheNearest(4,9);
+
+        System.out.println(testString.length());
+        System.out.println(Double.toString(test).length());
+
+        int i;
+        for(i= 0; i < (Double.toString(test).length()); i++) {
+            if(test.charAt(i) == "."){
+                System.out.println("this number is not an integer ");
+        } else {
+            System.out.println("This number is an integer ");
+        }
+    }
+}
 }
---
+++ timer_Config_Calculator.py
@@ -0,0 +1,9 @@
+'''
+David Harmon
+6-18-21
+
+This calculator is for the use of aiding in the configuration of a PIC16F883's
+timer prescale and postscale values. This will allow for the user to calculate
+a prescale and postscale value that will be a round value with refference to
+seconds.
+'''
